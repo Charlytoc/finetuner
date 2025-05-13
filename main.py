94ea8824f05bf174294bc19534a51e1eb592892f
Charlytoc <charlyjchaconc@gmail.com>
2025-05-13 17:54:21 -0500

    update host

--- main.py
+++ main.py
@@ -126,4 +126,4 @@ if ENVIRONMENT != "prod":
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=PORT, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT)
